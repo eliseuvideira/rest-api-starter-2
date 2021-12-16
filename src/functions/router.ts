3c7abd78f3b4622c97a327039d21690dbd75933d
Eliseu Videira <eliseuvideira1@gmail.com>
2021-12-15 22:00:55 -0300

    :construction: wip

--- src/functions/router.ts
+++ src/functions/router.ts
@@ -1,28 +1,67 @@
 import { RequestHandler, Router as ExpressRouter } from "express";
 
-export type Method = "get" | "post" | "put" | "patch" | "delete";
+export type Method = "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
+
+export const parse = (
+  method: Method,
+): "get" | "post" | "put" | "patch" | "delete" => {
+  switch (method) {
+    case "GET":
+      return "get";
+    case "POST":
+      return "post";
+    case "PUT":
+      return "put";
+    case "PATCH":
+      return "patch";
+    case "DELETE":
+      return "delete";
+  }
+};
 
 export type RouterHandler = (router: ExpressRouter) => ExpressRouter;
 
 export const handler =
-  (method: Method) =>
-  (path: string, ...handlers: RequestHandler[]): RouterHandler =>
+  (
+    method: Method,
+    path: string,
+    ...handlers: RequestHandler[]
+  ): RouterHandler =>
   (router: ExpressRouter) =>
-    router[method](path, ...handlers);
-
-export const Router = {
-  get: handler("get"),
-  post: handler("post"),
-  put: handler("put"),
-  patch: handler("patch"),
-  delete: handler("delete"),
-};
+    router[parse(method)](path, ...handlers);
 
 export const router = (...routes: RouterHandler[]) => {
   const router = ExpressRouter();
 
   for (const route of routes) {
     route(router);
+  }
+
+  return router;
+};
+
+export interface Router2Props {
+  path: string;
+  method: Method;
+  handlers: RequestHandler[];
+  docs?: {
+    tag: string;
+    summary: string;
+    responses: {
+      status: "200" | "201" | "204" | "default";
+      contentType: "application/json";
+      schema: string;
+    }[];
+  };
+}
+
+export const router2 = (routes: Router2Props[]) => {
+  const router = ExpressRouter();
+
+  for (const route of routes) {
+    const setHandler = handler(route.method, route.path, ...route.handlers);
+
+    setHandler(router);
   }
 
   return router;
--- src/routes/hello.ts
+++ src/routes/hello.ts
@@ -1,8 +1,26 @@
 import { hello, helloUsername } from "../endpoints/hello";
-import { Router, router } from "../functions/router";
+import { router2 } from "../functions/router";
 
-export const routes = router(
-  Router.get("/hello", hello),
-
-  Router.get("/hello/:username", helloUsername),
-);
+export const routes = router2([
+  {
+    path: "/hello",
+    method: "GET",
+    handlers: [hello],
+    docs: {
+      tag: "Hello",
+      summary: "Hello World",
+      responses: [
+        {
+          status: "200",
+          contentType: "application/json",
+          schema: "String",
+        },
+      ],
+    },
+  },
+  {
+    path: "/hello/:username",
+    method: "GET",
+    handlers: [helloUsername],
+  },
+]);
